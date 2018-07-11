3a7935b65eb547be563f79849060c29ac960c7e0
hanbing2207 <linhanbing01@outlook.com>
2018-07-11 22:43:14 +0200

    2

--- Algorithms/Lily_Homework.py
+++ Algorithms/Lily_Homework.py
@@ -30,71 +30,49 @@ def get_index(array, kind):
     return m_idx
 
 
-def swap(array1, array2, idx1, idx2):
-    t = array2[idx2]
-    array2[idx2] = array1[idx1]
-    array1[idx1] = t
-
-
-class stopit(Exception):
-    pass
-
-
-def split_n_sort(blocks):
-    """
-    split each block in blocks into two half, and swap max of block1 and min of block2
-    :param blocks: list of block
-    :return: 
-    """
-    global count
-    global nblocks
-    new_blocks = []
-    done = True
-    for block in blocks:
-        if len(block) == 1:
-            continue
-        demi = int(len(block)/2)
-        block1 = block[:demi]
-        block2 = block[demi:]
-
-        if max(block1) > min(block2):
-            count += 1
-            # print(count)
-            b1_max = get_index(block1, "max")
-            b2_min = get_index(block2, "min")
-            swap(block1, block2, b1_max, b2_min)
-            done = False
-        new_blocks.append(block1)
-        new_blocks.append(block2)
-        nblocks = new_blocks.copy()
-        print("done: ", done)
-    if not done:
-        print("newblock: ", new_blocks)
-        split_n_sort(new_blocks)
+def sort(arr, direction):
+    if direction == "forward":
+        smallest = min(arr[1:])
+        if arr[0] > smallest:
+            global count1
+            count1 += 1
+            min_idx = arr.index(smallest)
+            arr[min_idx] = arr[0]
+        return arr[1:]
     else:
-        raise stopit
-
-
-
+        smallest = min(arr[:-1])
+        if arr[-1] > smallest:
+            global count2
+            count2 += 1
+            min_idx = arr.index(smallest)
+            arr[min_idx] = arr[-1]
+        return arr[:-1]
 
 
 def lilysHomework(arr):
-    global count
-    count = 0
-    try:
-        blocks, count = split_n_sort([arr])
-    except stopit:
-        return count
+    arr2 = arr.copy()
+    global count1
+    count1 = 0
+
+    while len(arr) != 1:
+        print(len(arr))
+        arr = sort(arr, direction="forward")
+
+    global count2
+    count2 = 0
+    while len(arr2) != 1:
+        arr2 = sort(arr2, direction="backward")
+
+    return min(count1, count2)
 
 
 if __name__ == '__main__':
 
     n = 4
     blocks = []
-    file = open("input.txt")
+    file = open("input2.txt")
     strlist = file.read().split(" ")
     arr = [int(x) for x in strlist]
-    # arr = [2,5,3,1]
 
     result = lilysHomework(arr)
 
